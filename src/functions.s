4434e12b95dbb5651dde0fc72978766b186ab993
vzwGrey <vzwgrey@gmail.com>
2020-03-30 21:00:07 +0200

    feat: functions for drawing the paddles and ball

--- src/functions.s
+++ src/functions.s
@@ -8,7 +8,7 @@ clear_screen:
   ; 0x00 tells it to completely scroll everything out of view
   mov al, 0x00
   ; Set background color to black and text color to light grey
-  mov bh, 0x07
+  mov bh, 0x0F
   ; Store the coordinates of the top-left cell and the bottom-right
   ; cells in CX and DX respectively.
   mov cx, 0x0000
@@ -29,6 +29,19 @@ clear_screen:
 move_cursor:
   ; Move cursor function
   mov ah, 0x02
+  ; Page 0
+  mov bh, 0x00
+  int 0x10
+  ret
+
+; Write a character at the current cursor position,
+; the character to write is specified by the AL register.
+;
+; Clobbers: ah, bh
+write_char:
+  ; 0x0E is the function to print a character
+  ; from AL
+  mov ah, 0x0E
   ; Page 0
   mov bh, 0x00
   int 0x10
--- src/pong.s
+++ src/pong.s
@@ -1,12 +1,61 @@
 [bits 16]
 [org 0x7C00]
 
+; Vertical length of each player's paddle.
+PADDLE_SIZE equ 5 ; cells
+
+; Entry function (because it's at the beginning)
 start:
 	call clear_screen
+
+	mov ch, [LEFT_PLAYER_POS]
+	mov dl, 0
+	call draw_paddle
+	mov ch, [RIGHT_PLAYER_POS]
+	mov dl, 79
+	call draw_paddle
+	call draw_ball
 
 	cli
 	hlt
 	jmp start
+
+; Draw each of the player's paddles, starting at the row
+; specified by CH, then moves down PADDLE_SIZE cells to draw
+; a white box in each row inbetween.
+; The column to draw in is specified by DL.
+;
+; Clobbers: dh, ax, bh
+draw_paddle:
+	mov dh, ch
+	dec ch
+	add dh, PADDLE_SIZE
+.loop:
+	call move_cursor
+	mov al, 0xDB ; box character
+	call write_char
+
+	dec dh
+	cmp dh, ch
+	jne .loop
+.end:
+	ret
+
+; Draw the ball at the position specified by BALL_X and BALL_Y.
+;
+; Clobbers: dx, ax, bh
+draw_ball:
+	mov dh, [BALL_Y]
+	mov dl, [BALL_X]
+	call move_cursor
+	mov al, 0xDB ; box character
+	call write_char
+	ret
+
+LEFT_PLAYER_POS:  db 0
+RIGHT_PLAYER_POS: db 6
+BALL_X: db 40
+BALL_Y: db 12
 
 %include "functions.s"
 
